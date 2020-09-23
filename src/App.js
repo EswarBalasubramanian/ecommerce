e892f2f459f1140d5c21d7840fe5b60c3e817f02
unknown <eswarnaveen1@gmail.com>
2020-09-23 12:02:56 +0530

    added Tab menu

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 // import Card from "./components/Card/Card";
 import NavBar from "./components/Navbar/Navbar";
 import Carousel from "./components/Carousel/Carousel";
+import Tab from "./components/Tab/Tab";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Carousel />
+      <Tab />
     </div>
   );
 }
